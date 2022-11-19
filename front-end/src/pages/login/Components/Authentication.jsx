c53d6b0bf743cc1d72d1a8eb861b6663323472ab
Caelan Neumann <caelann2@gmail.com>
2022-11-19 14:10:15 -0700

    Update Authentication.jsx

--- front-end/src/pages/login/Components/Authentication.jsx
+++ front-end/src/pages/login/Components/Authentication.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import {Form, TextField} from "@mui/material";
+import React, {useState} from 'react';
+import {Form, Visibi FilledInput, IconButton, InputAdornment} from "@mui/material";
+import {Input} from "@mui/material";
 
 function Authentication() {
 
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        showPassword: '',
+    })
+
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleClickShowPassword = () => {
+        setValues({
+            ...values,
+            showPassword: !values.showPassword,
+        });
+    };
+
     return <>
-        <TextField/>
+
+        <Input
+            id="filled-adornment-password"
+            onChange={}
+            endAdornment={
+                <InputAdornment position="end">
+                    <IconButton
+                        aria-label="toggle password visibility"
+                        type={values.showPassword ? 'text' : 'password'}
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                    >
+                        {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                </InputAdornment>
+            }
+        />
     </>
 }
 
