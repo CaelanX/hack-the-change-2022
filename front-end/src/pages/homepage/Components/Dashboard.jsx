fccdd13f9972dc21458b90d976841717821db340
Lakshay Randhawa <lakshayrandhawa@gmail.com>
2022-11-19 15:47:32 -0700

    dashboard.jsx

---
+++ front-end/src/pages/homepage/Components/Dashboard.jsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "documentName", headerName: "Name", width: 130 },
+  {
+    field: "dateIssued",
+    headerName: "Issued On",
+    type: "number",
+    width: 90,
+  },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+export default function Dashboard() {
+  return (
+    <div style={{ height: "100%", width: "50%", margin: "auto"}}>
+      <DataGrid
+      style={{padding : "10px 25px"}}
+        rows={rows}
+        columns={columns}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        autoHeight={true}
+        checkboxSelection
+        density="comfortable"
+        disableExtendRowFullWidth={false}
+        hideFooterPagination={true}
+      />
+    </div>
+  );
+}
--- front-end/src/pages/homepage/index.jsx
+++ front-end/src/pages/homepage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Dashboard from './Components/Dashboard'
 import Navbar from './Components/Navbar'
 
 function Homepage() {
   return (
     <div>
         <Navbar />
+        <Dashboard />
     </div>
   )
 }
