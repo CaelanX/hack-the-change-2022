98c96329d1a8a7fda3851a88f713d0e936115fff
Caelan Neumann <caelann2@gmail.com>
2022-11-20 02:32:24 -0700

    Update index.js
    setup routes

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import Authentication from "./pages/login/Authentication";
+import Homepage from "./pages/homepage";
+import CreateCredential from "./pages/CreateCredential/CreateCredential";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Authentication/>}/>
+        <Route path="homepage" element={<Homepage/>}/>
+        <Route path="credential" element={<CreateCredential/>}/>
+
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
