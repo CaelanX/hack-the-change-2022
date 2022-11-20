fa957c411d556764fb05830daa74d677e59c84fc
Caelan Neumann <caelann2@gmail.com>
2022-11-20 09:46:12 -0700

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import Authentication from "./pages/login/Authentication";
 import Homepage from "./pages/homepage";
 import CreateCredential from "./pages/CreateCredential/CreateCredential";
+import LoginCard from "./pages/login/Components/LoginCard";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Authentication/>}/>
+        <Route path="/" index element={<LoginCard/>}/>
         <Route path="homepage" element={<Homepage/>}/>
         <Route path="credential" element={<CreateCredential/>}/>
       </Routes>
