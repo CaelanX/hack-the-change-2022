2b9daeab2d3909d2cefad038d26acc0a5adae3d3
Caelan Neumann <caelann2@gmail.com>
2022-11-19 20:13:19 -0700

    Update Authentication.jsx

--- front-end/src/pages/login/Authentication.jsx
+++ front-end/src/pages/login/Authentication.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import AuthenticationCard from "./Components/AuthenticationCard";
+import RegisterCard from "./Components/RegisterCard";
+import {Button, Typography} from "@mui/material";
 
 
 
 function Authentication() {
+  const [registration, setRegistration] = useState(false)
+  const onClick = () => {
+    setRegistration(!registration)
+  }
+
   return <div className="Authentication-Page">
-    
+    <Typography className='authentication-title' variant='h1' >Uni<span>Creds</span></Typography>
+    {registration ? <RegisterCard/> : <AuthenticationCard/>}
+    {registration ? <></> :
+    <Button variant={'contained'} onClick={onClick}>Register</Button> }
 
   </div>
 }
