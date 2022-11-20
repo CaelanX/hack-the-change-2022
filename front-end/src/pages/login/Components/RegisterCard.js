29c368e1e3bcca5a7bd64fc97fd05ffe4c0472f3
Caelan Neumann <caelann2@gmail.com>
2022-11-20 11:10:05 -0700

    Update RegisterCard.js

--- front-end/src/pages/login/Components/RegisterCard.js
+++ front-end/src/pages/login/Components/RegisterCard.js
@@ -7,9 +7,10 @@ import {Mail, Person} from "@mui/icons-material";
 import {collection, doc, setDoc} from "firebase/firestore";
 import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth'
 import "./RegisterCard.css"
+import {useNavigate} from "react-router-dom";
 
 const RegisterCard = () => {
-    
+    const navigator = useNavigate()
     const auth = () => {
         return getAuth();
     }
@@ -23,6 +24,9 @@ const RegisterCard = () => {
                 console.log(userCredential)
                 }
             )
+    }
+    const handleClick = () => {
+        navigator("/")
     }
 
     const validationSchema = yup.object({
@@ -162,8 +166,14 @@ const RegisterCard = () => {
                            value={formik.values.password}
                     />
                 </FormControl>
-                <Button form="register-form" onClick={formik.submitForm} sx={{backgroundColor: "darkred"}} variant='contained'>Register</Button>
-
+                <Stack spacing={2} direction={"row"}>
+                    <Button form="register-form" onClick={formik.submitForm} sx={{backgroundColor: "darkred"}} variant='contained'>Register</Button>
+                    <a
+                        onClick={handleClick}
+                    >
+                        <Typography style={{ color: "blue" }}>Back to Login</Typography>
+                    </a>{" "}
+                </Stack>
             </Stack>
             </FormikProvider>
         </form>
